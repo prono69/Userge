a752b6532ab22ab2d751479cc30e00ab5528c234
prono69 <faltu9851@gmail.com>
2020-09-09 01:34:38 +0530

    added some modules from userge-x

---
+++ userge/plugins/bot/bot_pm.py
@@ -0,0 +1,131 @@
+# Copyright (C) 2020 BY - GitHub.com/code-rgb [TG - @deleteduser420]
+# All rights reserved.
+"""Module that handles Bot's Pm"""
+
+from userge import userge, Message, Config, get_collection
+from pyrogram.types import (  
+     InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery )
+from pyrogram import filters
+from pyrogram.errors.exceptions import FileIdInvalid, FileReferenceEmpty
+from pyrogram.errors.exceptions.bad_request_400 import BadRequest
+from datetime import date
+import asyncio
+
+LOG = userge.getLogger("Bot_PM")
+CHANNEL = userge.getCLogger("Bot_PM")
+
+BOT_BAN = get_collection("BOT_BAN")
+BOT_START = get_collection("BOT_START")
+LOGO_ID, LOGO_REF = None, None
+# https://github.com/UsergeTeam/Userge-Assistant/.../alive.py#L41
+# refresh file id and file reference from TG server
+
+
+if Config.BOT_TOKEN and Config.OWNER_ID:
+    if Config.HU_STRING_SESSION:
+        ubot = userge.bot
+    else:
+        ubot = userge
+
+
+    @ubot.on_message(filters.private & filters.command("start"))
+    async def start_bot(_, message: Message):
+        bot = await userge.bot.get_me()
+        master = await userge.get_me()
+        u_id = message.from_user.id
+        found = await BOT_BAN.find_one({'user_id': u_id})
+        if found:
+            return
+        f_name = message.from_user.first_name
+        u_n = master.username
+        
+        hello = f"""
+Hello {f_name},
+Nice To Meet You! I'm **{bot.first_name}** A Bot. 
+
+        <i><b>Powered by</i> PEPESERGE</b>
+
+<i>You Can Contact</i> My Master : **{master.first_name}**
+<i>And Check The Repo For More Info.</i>
+"""
+        if Config.BOT_FORWARDS:          
+            hello += "\n<b>NOTE : </b> "
+            hello += "**Bot Forwarding is** :  ☑️ `Enabled`\n"
+            hello += "<i>All your messages here will be forwared to</i> My MASTER"
+        if u_id != Config.OWNER_ID:
+            found = await BOT_START.find_one({'user_id': u_id})
+            if not found:
+                today = date.today()
+                d2 = today.strftime("%B %d, %Y")
+                start_date = d2.replace(',', '')
+                u_n = master.username
+                await asyncio.gather(
+                    BOT_START.insert_one(
+                        {'firstname': f_name, 'user_id': u_id, 'date': start_date}),
+                await CHANNEL.log(f"A New User Started your Bot \n\n• <i>ID</i>: `{u_id}`\n   <b>Name</b>: {f_name}")
+                )
+        try:
+            if LOGO_ID:
+                await sendit(message, LOGO_ID, LOGO_REF, hello, u_n)
+            else:
+                await refresh_id()
+                await sendit(message, LOGO_ID, LOGO_REF, hello, u_n)
+        except (FileIdInvalid, FileReferenceEmpty, BadRequest):
+            await refresh_id()
+            await sendit(message, LOGO_ID, LOGO_REF, hello, u_n)
+
+
+    async def refresh_id():
+        global LOGO_ID, LOGO_REF
+        vid = (await ubot.get_messages('useless_x', 2)).video
+        LOGO_ID = vid.file_id
+        LOGO_REF = vid.file_ref
+
+
+    async def sendit(message, fileid, fileref, caption, u_n):
+        await ubot.send_video(
+            chat_id=message.chat.id,
+            video=fileid, 
+            file_ref=fileref,
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("CONTACT", url=f"t.me/{u_n}"),
+                InlineKeyboardButton("REPO", url="https://github.com/prono69/Userge")],
+                [InlineKeyboardButton("➕ ADD TO GROUP", callback_data="add_to_grp")
+                ]]
+            )
+        )
+
+
+    @ubot.on_callback_query(filters.regex(pattern=r"^add_to_grp$"))
+    async def add_to_grp(_, callback_query: CallbackQuery): 
+        u_id = callback_query.from_user.id 
+        if u_id == Config.OWNER_ID:
+            botname = (await ubot.get_me()).username
+            msg = "**🤖 Add Your Bot to Group** \n\n <u>Note:</u>  <i>Admin Privilege Required !</i>"
+            add_bot = f"http://t.me/{botname}?startgroup=start"
+
+            buttons = [[InlineKeyboardButton("➕ PRESS TO ADD", url=add_bot)]]
+            await callback_query.edit_message_text(
+                    msg,
+                    reply_markup=InlineKeyboardMarkup(buttons)
+            )
+        else:
+            await callback_query.answer("ONLY MY MASTER CAN DO THAT ! \n\n 𝘿𝙚𝙥𝙡𝙤𝙮 𝙮𝙤𝙪𝙧 𝙤𝙬𝙣 𝙐𝙎𝙀𝙍𝙂𝙀 !", show_alert=True)
+ 
+ 
+@userge.on_cmd("bot_users", about={
+    'header': "Get a list Active Users Who started your Bot",
+    'examples': "{tr}bot_users"},
+    allow_channels=False)
+async def bot_users(message: Message):
+    """Users Who Stated Your Bot by - /start"""
+    msg = ""
+    async for c in BOT_START.find():  
+        msg += f"• <i>ID:</i> <code>{c['user_id']}</code>\n   <b>Name:</b> {c['firstname']},  <b>Date:</b> `{c['date']}`\n"
+    await message.edit_or_send_as_file(
+        f"<u><i><b>Bot PM Userlist</b></i></u>\n\n{msg}" if msg else "`Nobody Does it Better`")
+
+
+
+
---
+++ userge/plugins/bot/secret.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2020 BY - GitHub.com/code-rgb [TG - @deleteduser420]
+# All rights reserved.
+
+from userge import userge, Config, get_collection, Message
+from pyrogram.types import CallbackQuery
+from pyrogram import filters
+import json
+import os
+
+SECRETS = "userge/xcache/secret.txt"
+
+
+if Config.BOT_TOKEN and Config.OWNER_ID:
+    if Config.HU_STRING_SESSION:
+        ubot = userge.bot
+    else:
+        ubot = userge
+
+       
+    @ubot.on_callback_query(filters.regex(pattern=r"^secret_(.*)"))
+    async def alive_callback(_, c_q: CallbackQuery):
+        msg_id = c_q.matches[0].group(1)
+        if os.path.exists(SECRETS):
+            view_data = json.load(open(SECRETS))
+            sender = await userge.get_me()
+            msg = f"🔓 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗳𝗿𝗼𝗺: {sender.first_name}"
+            if sender.last_name:
+                msg += f" {sender.last_name}\n"
+            else:
+                msg += "\n"
+            data = view_data[msg_id]
+            receiver =  data['user_id']
+            msg += data['msg']
+            u_id = c_q.from_user.id 
+            if u_id == Config.OWNER_ID or u_id == receiver:
+                await c_q.answer(msg, show_alert=True)
+            else:
+                await c_q.answer("This Message is Confidential 😈", show_alert=True)
+        else:
+            await c_q.answer("This message doesn't exist anymore", show_alert=True)
+
+
+@userge.on_cmd("secret", about={
+    'header': "for help do .secret"})
+async def secret_(message: Message):
+    text = "**IN INLINE BOT**\n\n"
+    text += "secret [username OR userid] \"Your Secret Message\""
+    await message.edit(text, del_in=20)
+    
